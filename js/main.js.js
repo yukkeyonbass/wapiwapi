b4097c1cf40a325a696a8edee1c7b29bbd315659
yukkeyonbass <105627702+yukkeyonbass@users.noreply.github.com>
2022-05-27 15:59:56 +0900

    Add files via upload

---
+++ js/main.js.js
@@ -0,0 +1,29 @@
+'use strict';
+
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement('section');
+      section.classList.add('panel');
+
+      this.img = document.createElement('img');
+      this.img.src = 'img/wapi.jpeg';
+
+      this.stop = document.createElement('div');
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+    }
+  }
+
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
+}
